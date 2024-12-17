a7b27dd0c3c8a0a7f04a98f31e21b4f79cd97b43
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2024-12-17 01:22:56 +0000

    Primeiro teste de API

---
+++ test/integrations/api/v1/status/get.test.js
@@ -0,0 +1,6 @@
+test("Get to /api/v1/status should return 200", async () => {
+  const response = await fetch(
+    "https://sturdy-pancake-4rjv7g9qjv3j6xq-3000.app.github.dev/api/v1/status",
+  );
+  expect(response.status).toBe(200);
+});
